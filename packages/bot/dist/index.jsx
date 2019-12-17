aa58029e08d1ace8d922eeeaab858863c1d01482
lezi.chen <pleasurazy@gmail.com>
2019-12-18 02:16:13 +0800

    feat(bot): LINE works, 跟隨 guide 使用 .env 接著 `run dev` 複制 webhook_url 到 LINE@，關閉 LINE 的初始自動回應訊息

--- packages/bot/dist/index.jsx
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function App(context) {
-    await context.sendText('Welcome to Bottender');
-}
-exports.default = App;
