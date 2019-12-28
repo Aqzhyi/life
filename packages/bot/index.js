49a10b2239a4d2534aa77c33bc72e829964eedc6
lezi.chen <pleasurazy@gmail.com>
2019-12-29 07:58:07 +0800

    build: 使用 webpack

--- packages/bot/index.js
+++ packages/bot/index.js
@@ -1,1 +1,1 @@
-module.exports = require('./dist/index.jsx').default
+module.exports = require('./dist/bundle.jsx').default
---
+++ packages/bot/webpack.config.ts
@@ -0,0 +1,57 @@
+import { Configuration } from 'webpack'
+import path from 'path'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+
+const configration: Configuration = {
+  /** TODO: cheap-module-eval-source-map is no suit for production */
+  devtool: 'cheap-module-eval-source-map',
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src/index'),
+  },
+  output: {
+    filename: 'bundle.jsx',
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: [/node_modules/, path.join(__dirname, '../app')],
+        loader: 'eslint-loader',
+        options: {
+          configFile: path.resolve(__dirname, '.eslintrc.js'),
+        },
+        test: /\.tsx?$/,
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: { node: 'current' } }],
+              ],
+              plugins: [
+                '@babel/plugin-proposal-optional-chaining',
+                '@babel/plugin-proposal-nullish-coalescing-operator',
+              ],
+            },
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+}
+
+export default configration
