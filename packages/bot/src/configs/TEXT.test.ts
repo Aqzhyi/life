428cd8ce1e46d54fded7f01fef87f08ab67d0819
hilezi <hilezi.chen@gmail.com>
2020-01-19 04:23:53 +0800

    test(bot): 更完整查詢直播指令測試

--- packages/bot/src/configs/TEXT.test.ts
+++ packages/bot/src/configs/TEXT.test.ts
@@ -1,31 +1,44 @@
 import { QueryTwitchStreamsText } from '@/configs/TEXT'
 import { createCommandText } from '@/utils/createCommandText'
+import { createDirectlyText } from '@/utils/createDirectlyText'
 
 describe('指令正規', () => {
   it('查詢直播頻道: [!！]$(直播|live)', () => {
-    const command = createCommandText(QueryTwitchStreamsText)
-    expect(command.test('!直播')).toBe(true)
-    expect(command.test('！直播')).toBe(true)
-    expect(command.test('!live')).toBe(true)
-    expect(command.test('！live')).toBe(true)
-    expect(command.test('！直播 魔獸')).toBe(true)
-    expect(command.test('！直播 星海')).toBe(true)
-    expect(command.test('！直播 dota')).toBe(true)
-    expect(command.test('！直播魔獸')).toBe(true)
-    expect(command.test('！直播星海')).toBe(true)
-    expect(command.test('！直播dota')).toBe(true)
-    expect(command.test('！直播lol')).toBe(true)
-    expect(command.test('！直播LOL')).toBe(true)
-    expect(command.test('！直播WOW')).toBe(true)
-    expect(command.test('！直播OW')).toBe(true)
-    expect(command.test('！直播 0f8071555f7d')).toBe(true)
+    const commands = [
+      createDirectlyText(QueryTwitchStreamsText),
+      createCommandText(QueryTwitchStreamsText),
+    ]
 
-    expect(command.test('$300')).toBe(false)
-    expect(command.test('$直播dota')).toBe(false)
-    expect(command.test('$直播lol')).toBe(false)
-    expect(command.test('$直播LOL')).toBe(false)
-    expect(command.test('$直播WOW')).toBe(false)
-    expect(command.test('$直播OW')).toBe(false)
-    expect(command.test('$直播 0f8071555f7d')).toBe(false)
+    for (const command of commands) {
+      expect(command.test('!直播')).toBe(true)
+      expect(command.test('！直播')).toBe(true)
+      expect(command.test('!live')).toBe(true)
+      expect(command.test('！live')).toBe(true)
+      expect(command.test('！直播 魔獸')).toBe(true)
+      expect(command.test('！直播 星海')).toBe(true)
+      expect(command.test('！直播 dota')).toBe(true)
+      expect(command.test('！直播魔獸')).toBe(true)
+      expect(command.test('！直播星海')).toBe(true)
+      expect(command.test('！直播dota')).toBe(true)
+      expect(command.test('！直播lol')).toBe(true)
+      expect(command.test('！直播LOL')).toBe(true)
+      expect(command.test('！直播WOW')).toBe(true)
+      expect(command.test('！直播OW')).toBe(true)
+      expect(command.test('！直播 0f8071555f7d')).toBe(true)
+
+      expect(command.test('$300')).toBe(false)
+      expect(command.test('$直播dota')).toBe(false)
+      expect(command.test('$直播lol')).toBe(false)
+      expect(command.test('$直播LOL')).toBe(false)
+      expect(command.test('$直播WOW')).toBe(false)
+      expect(command.test('$直播OW')).toBe(false)
+      expect(command.test('$直播 0f8071555f7d')).toBe(false)
+
+      expect(command.exec('!直播英雄')?.groups?.inputKeyword).toBe('英雄')
+      expect(command.exec('!直播dota')?.groups?.inputKeyword).toBe('dota')
+      expect(command.exec('!直播星海')?.groups?.inputKeyword).toBe('星海')
+      expect(command.exec('!livesc')?.groups?.inputKeyword).toBe('sc')
+      expect(command.exec('!livewow')?.groups?.inputKeyword).toBe('wow')
+    }
   })
 })
