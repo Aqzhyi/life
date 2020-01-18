9a79b9c7283cdde70c52c6ab81731d9c38b29c75
hilezi <hilezi.chen@gmail.com>
2020-01-18 14:10:10 +0800

    refactor(bot/twitch): 記錄 API 用量與錯誤

---
+++ packages/bot/src/lib/google-analytics/EventCategory.ts
@@ -0,0 +1,4 @@
+export enum EventCategory {
+  API = 'API',
+  LINEBOT = 'LINEBOT',
+}
--- packages/bot/src/lib/twitch/axiosAPI.tsx
+++ packages/bot/src/lib/twitch/axiosAPI.tsx
@@ -1,6 +1,8 @@
 import originalAxios from 'axios'
 import { decamelizeKeys, camelizeKeys } from 'humps'
 import ow from 'ow'
+import { gaAPI } from '@/lib/google-analytics/gaAPI'
+import { EventCategory } from '@/lib/google-analytics/EventCategory'
 
 ow(process.env.TWITCH_CLIENT_ID, ow.string.nonEmpty)
 
@@ -19,5 +21,34 @@ axiosAPI.interceptors.request.use(config => {
 
 axiosAPI.interceptors.response.use(response => {
   response.data = camelizeKeys(response.data)
+
+  gaAPI.send({
+    ec: EventCategory.API,
+    ea: `使用/twitch${response.config.url}`,
+    el: JSON.stringify({
+      config: response.config,
+    }),
+  })
+
+  if (response.status >= 400 && response.status < 500) {
+    gaAPI.send({
+      ec: EventCategory.API,
+      ea: 'twitch/前端錯誤',
+      el: JSON.stringify({
+        config: response.config,
+      }),
+    })
+  }
+
+  if (response.status >= 500) {
+    gaAPI.send({
+      ec: EventCategory.API,
+      ea: 'twitch/後端錯誤',
+      el: JSON.stringify({
+        config: response.config,
+      }),
+    })
+  }
+
   return response
 })
