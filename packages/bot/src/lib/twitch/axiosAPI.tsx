a76333936bf7e881e6c62c8fe6f676accb74a328
hilezi <hilezi.chen@gmail.com>
2020-01-19 20:39:05 +0800

    refactor(bot/twitch): 移除敏感資訊

--- packages/bot/src/lib/twitch/axiosAPI.tsx
+++ packages/bot/src/lib/twitch/axiosAPI.tsx
@@ -3,13 +3,17 @@ import { decamelizeKeys, camelizeKeys } from 'humps'
 import ow from 'ow'
 import { gaAPI } from '@/lib/google-analytics/gaAPI'
 import { EventCategory } from '@/lib/google-analytics/EventCategory'
+import { omit } from 'lodash'
 
 ow(process.env.TWITCH_CLIENT_ID, ow.string.nonEmpty)
+
+const CLIENT_ID_KEY = 'Client-Id'
+const AUTHORIZATION_KEY = 'Authorization'
 
 export const axiosAPI = originalAxios.create({
   baseURL: 'https://api.twitch.tv/helix',
   headers: {
-    ['Client-Id']: process.env.TWITCH_CLIENT_ID,
+    [CLIENT_ID_KEY]: process.env.TWITCH_CLIENT_ID,
   },
   responseType: 'json',
 })
@@ -22,11 +26,21 @@ axiosAPI.interceptors.request.use(config => {
 axiosAPI.interceptors.response.use(response => {
   response.data = camelizeKeys(response.data)
 
+  const headers = omit(response.config.headers, [
+    CLIENT_ID_KEY,
+    AUTHORIZATION_KEY,
+  ])
+
+  const config = {
+    ...response.config,
+    headers,
+  }
+
   gaAPI.send({
     ec: EventCategory.API,
     ea: `使用/twitch${response.config.url}`,
     el: {
-      config: response.config,
+      config,
     },
   })
 
@@ -35,7 +49,7 @@ axiosAPI.interceptors.response.use(response => {
       ec: EventCategory.API,
       ea: 'twitch/前端錯誤',
       el: {
-        config: response.config,
+        config,
       },
     })
   }
@@ -45,7 +59,7 @@ axiosAPI.interceptors.response.use(response => {
       ec: EventCategory.API,
       ea: 'twitch/後端錯誤',
       el: {
-        config: response.config,
+        config,
       },
     })
   }
