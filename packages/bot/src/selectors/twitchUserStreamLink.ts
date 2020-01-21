251d8901e4f2769ffe8d7a7a0048fb930041acaa
hilezi <hilezi.chen@gmail.com>
2020-01-21 09:42:50 +0800

    refactor(bot): missing comment

--- packages/bot/src/selectors/twitchUserStreamLink.ts
+++ packages/bot/src/selectors/twitchUserStreamLink.ts
@@ -1,5 +1,6 @@
 import ow from 'ow'
 
+/** 拿到直播主網址，適合用於前端開啟連結 */
 export const twitchUserStreamLink = (thumbnailUrl: string) => {
   ow(thumbnailUrl, ow.string.matches(/^https[\s\S]+live_user_[\s\S]+-/i))
   const urlId = /live_user_(.*?)-/i.exec(thumbnailUrl)?.[1]
