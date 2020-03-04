83717395bb76050657084cb65384792cb4a36df7
hilezi <pleasurazy@gmail.com>
2020-03-04 21:41:10 +0800

    refactor(bot): LINE sendFlex 避免洗畫面，最多送十次

--- packages/bot/src/lib/bottender-toolkit/sendFlex.ts
+++ packages/bot/src/lib/bottender-toolkit/sendFlex.ts
@@ -21,6 +21,7 @@ export const sendFlex = (
   const sendPromises: NonNullable<ReturnType<typeof context['sendText']>>[] = []
   const IS_LINE_CONTEXT = isLineContext(context)
   const PER_CHUNK = 10
+  const MAX_SEND = 10
 
   if (IS_LINE_CONTEXT) {
     try {
@@ -42,7 +43,11 @@ export const sendFlex = (
            */
           const dataChunks = chunk(props.bubbles, PER_CHUNK)
 
-          for (const dataChunk of dataChunks) {
+          const isReachingMax = dataChunks.length > MAX_SEND
+
+          for (const dataChunk of (isReachingMax &&
+            dataChunks.splice(0, MAX_SEND)) ||
+            dataChunks) {
             if (dataChunk.length) {
               const promise = context.sendFlex(
                 props.alt || '那個機器人說話了',
@@ -53,6 +58,13 @@ export const sendFlex = (
               )
               promise && sendPromises.push(promise)
             }
+          }
+
+          if (isReachingMax) {
+            const promise = context.sendText(
+              '訊息太多，未避免洗畫面，在此截斷。',
+            )
+            promise && sendPromises.push(promise)
           }
         } catch (error) {
           console.error(error.message)
